e4ac7f31757730e000e6390afc8d81e9e3eb246e
Crabby Disk <yfmao2010@hotmail.com>
2024-11-08 12:26:27 -0500

    Use luasnip

--- config/lsp.nix
+++ config/lsp.nix
@@ -20,18 +20,37 @@
 
   plugins.lsp-format.enable = true;
 
+  plugins.luasnip.enable = true;
+
   plugins.cmp = {
     enable = true;
     autoEnableSources = true;
     settings.sources = [
       { name = "nvim_lsp"; }
       { name = "path"; }
-      { name = "buffer"; }
+      { name = "luasnip"; }
     ];
+    settings.snippet.expand = "function(args) require('luasnip').lsp_expand(args.body) end";
     settings.mapping = {
       "<CR>" = "cmp.mapping.confirm({ select = true })";
-      "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
-      "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+      "<Tab>" = ''cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.locally_jumpable(1) then
+        luasnip.jump(1)
+      else
+        fallback()
+      end
+    end, { "i", "s" })'';
+      "<S-Tab>" = ''cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.locally_jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" })'';
     };
   };
 }
